780e4499c81446c3cc6f5d88dc9c89a4af1d7ab5
nesterok-d <nesr.daria.nest@gmail.com>
2022-07-12 14:40:05 +0700

    обновление

--- src/main/java/pages/home/OuterWearWomen.java
+++ src/main/java/pages/home/OuterWearWomen.java
@@ -2,15 +2,18 @@ package pages.home;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import pages.base.BasePage;
 
+import java.util.List;
 import java.util.Objects;
 
 
 public class OuterWearWomen extends BasePage{
     public OuterWearWomen(WebDriver driver) {super(driver);}
 
-    private final By sort = By.cssSelector("div.select__container--city");
+    private final By sortButtun = By.cssSelector("div.select__container--city");
+    private final By sort = By.cssSelector("li.js-catalog-sort-btn");
     private final By sortNewIncr = By.cssSelector("li.js-catalog-sort-btn[data-sort='new-desc']");
     private final By sortNewDecr = By.cssSelector("li.js-catalog-sort-btn[data-sort='new-asc']");
     private final By sortPriceIncr = By.cssSelector("li.js-catalog-sort-btn[data-sort='price-desc']");
@@ -19,23 +22,26 @@ public class OuterWearWomen extends BasePage{
     private final By sortPopularDecr = By.cssSelector("li.js-catalog-sort-btn[data-sort='popular-asc']");
     private final By catalogTitle = By.cssSelector("h1.catalog__title");
 
-    public void sortListOfProduct() throws InterruptedException {
+    public void sortListOfProduct(String expectedSortType, String expectedSortTitle) throws InterruptedException {
         By[] listSort = new By[]{sortNewIncr, sortNewDecr, sortPriceIncr,sortPriceDecr,sortPopularIncr,sortPopularDecr};
-        String [] expectedSortType = {"По новинкам ↓", "По новинкам ↑", "По возрастанию цены ↓", "По убыванию цены ↑", "По популярности ↓", "По популярности ↑"};
-        String [] expectedSortTitle ={"Женская верхняя одежда, новая коллекция", "Женская верхняя одежда, основная коллекция", "Женская верхняя одежда, недорого", "Женская верхняя одежда, премиум", "Женская верхняя одежда, смелое", "Женская верхняя одежда, популярное"};
-
         int i = 0;
+        driver.findElement(sortButtun).click();
+        Thread.sleep(2000);
         for (By sortType : listSort){
-            driver.findElement(sort).click();
-            Thread.sleep(2000);
-            String actualSortType = driver.findElement(sortType).getAttribute("value");
-            assert Objects.equals(actualSortType, expectedSortType[i]);
-            driver.findElement(sortType).click();
-            String actualSortTitle = driver.findElement(catalogTitle).getText();
-            assert Objects.equals(actualSortTitle, expectedSortTitle[i]);
-            i++;
-        }
+            String actualSortType = driver.findElement(sortType).getText();
+            if (actualSortType.equals(expectedSortType)){
+                driver.findElement(sortType).click();
+                Thread.sleep(2000);
+                String actualSortTitle = driver.findElement(catalogTitle).getText();
+                assert Objects.equals(actualSortTitle, expectedSortTitle);
+                i++;
+                Thread.sleep(2000);
+            }else{
+                break;
+            }
 
+        }
+        if (i!=1){System.out.println("нет категории " + expectedSortType);}
     }
 
 
--- src/test/java/tests/womenouterwear/WomenOuterwearTest.java
+++ src/test/java/tests/womenouterwear/WomenOuterwearTest.java
@@ -10,9 +10,17 @@ import java.util.List;
 public class WomenOuterwearTest extends BaseTest {
 
     @Test
-    public void sortList() throws InterruptedException { //полуение и проверка списка категорий первогу уровня
+    public void sortList() throws InterruptedException { //получение и проверка списка категорий первого уровня
         basePage.open("https://lgcity.ru/outerwear/women/");
-        outerWearWomen.sortListOfProduct();
+        Thread.sleep(2000);
+        String [] expectedSortType = {"По новинкам ↓", "По новинкам ↑", "По возрастанию цены ↓ ", "По убыванию цены ↑", "По популярности ↓", "По популярности ↑"};
+        String [] expectedSortTitle ={"Женская верхняя одежда, новая коллекция", "Женская верхняя одежда, основная коллекция", "Женская верхняя одежда, недорого", "Женская верхняя одежда, премиум", "Женская верхняя одежда, смелое", "Женская верхняя одежда, популярное"};
+        int nom = 6;
+        for (int n=0; n<nom; n++){
+            outerWearWomen.sortListOfProduct(expectedSortType[n],expectedSortTitle[n]);
+
+        }
+
     }
 
 
